1e9df76674d2b5e45dd7dc46b339540f4a456692
shakedm <shakedmigdal@campus.technion.ac.il>
2017-05-29 13:16:41 +0300

    new files

--- company.h
+++ company.h
@@ -1,8 +1,16 @@
-//
-// Created by shaked on 5/29/2017.
-//
-
 #ifndef HW3WET_COMPANY_H
 #define HW3WET_COMPANY_H
+
+#include "room.h"
+#include "mtm_ex3.h"
+
+typedef struct company_t* Company;
+
+MtmErrorCode initCompany(Company company, char* email, TechnionFaculty faculty);
+MtmErrorCode resetCompany(Company company);
+MtmErrorCode getCompanyFaculty(Company company, TechnionFaculty* faculty);
+MtmErrorCode getCompanyRevenue(Company company, int* revenue);
+MtmErrorCode addRoomCompany(Company company, Room* room);
+
 
 #endif //HW3WET_COMPANY_H
