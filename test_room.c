354892726d81f2a7c4f941f082a331c20702afac
shakedm <shakedmigdal@campus.technion.ac.il>
2017-06-06 18:25:37 +0300

    finish tests for room

--- test_room.c
+++ test_room.c
@@ -8,11 +8,11 @@ static bool testInitRoom(){
     ASSERT_TEST(initRoom(NULL,NULL,NULL,NULL,NULL,NULL,NULL)
                 == MTM_INVALID_PARAMETER);
     ASSERT_TEST(initRoom(&room,"asd",22,23,"10-23",3,40)==MTM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"100-20",2,20)
+    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"100-20",2,20)
                 ==MTM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"20-10",2,20)
+    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"20-10",2,20)
                 ==MTM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"10-20",2,20)==MTM_SUCCESS);
+    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"10-20",2,20)==MTM_SUCCESS);
     resetRoom(room);
     destroyRoom(room);
 }
