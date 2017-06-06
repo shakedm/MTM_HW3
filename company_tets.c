18cc6f09ac5fbbe82672cf4238051efc7d2c1343
shakedm <shakedmigdal@campus.technion.ac.il>
2017-06-06 15:05:48 +0300

    test

--- company_tets.c
+++ company_tets.c
@@ -1,8 +1,16 @@
 #include "test_utilities.h"
 #include "company.h"
+#include "usefullFunctions.h"
 
 #include <stdbool.h>
 #include <assert.h>
 
+static bool testInitCompany(){
+    Company* company;
+    char* email= {"notleagal"};
+    char* email2={"is@legal"};
+    ASSERT_TEST(initCompany(NULL,NULL,NULL)==MTM_NULL_PARAMETER);
+    ASSERT_TEST(initCompany(company,email,UNKNOWN)==MTM_INVALID_PARAMETER);
 
+}
 
