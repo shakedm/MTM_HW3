1e4aa31fd30ce222a902052abc78ed01dcff194e
shakedm <shakedmigdal@campus.technion.ac.il>
2017-06-12 12:08:54 +0300

    TEST

--- test_room.c
+++ test_room.c
@@ -6,12 +6,12 @@ static bool testInitRoom(){
     Room room= createRoom();
     ASSERT_TEST(initRoom(NULL,NULL,0,0,NULL,0,0)
                 == ROOM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(&room,"asd",22,23,"10-23",3,40)==ROOM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"100-20",2,20)
+    ASSERT_TEST(initRoom(room,"asd",22,23,"10-23",3,40)==ROOM_INVALID_PARAMETER);
+    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"100-20",2,20)
                 ==ROOM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"20-10",2,20)
+    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"20-10",2,20)
                 ==ROOM_INVALID_PARAMETER);
-    ASSERT_TEST(initRoom(&room,"sha@s",23,2 ,"10-20",2,20)==ROOM_SUCCESS);
+    ASSERT_TEST(initRoom(room,"sha@s",23,2 ,"10-20",2,20)==ROOM_SUCCESS);
     resetRoom(room);
     destroyRoom(room);
     return true;
@@ -23,7 +23,7 @@ static bool testResetRoom(){
 
 static bool testGetId(){
     Room room= createRoom();
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,20);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,20);
     ASSERT_TEST(roomGetId(room)==23);
     resetRoom(room);
     ASSERT_TEST(roomGetId(room)!=23);
@@ -32,7 +32,7 @@ static bool testGetId(){
 }
 static bool testGetNumPpl(){
     Room room= createRoom();
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,20);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,20);
     ASSERT_TEST(roomGetNumPpl(room)==2);
     resetRoom(room);
     ASSERT_TEST(roomGetNumPpl(room)!=2);
@@ -43,9 +43,9 @@ static bool testGetDiffucalty(){
     Room room= createRoom();
     Room room1= createRoom();
     Room room2= createRoom();
-    initRoom(&room,"sha@s",21,2 ,"10-20",2,20);
-    initRoom(&room1,"sha@sa",22,2 ,"10-20",1,20);
-    initRoom(&room2,"sha@sass",24,2 ,"10-20",10,20);
+    initRoom(room,"sha@s",21,2 ,"10-20",2,20);
+    initRoom(room1,"sha@sa",22,2 ,"10-20",1,20);
+    initRoom(room2,"sha@sass",24,2 ,"10-20",10,20);
     ASSERT_TEST(roomGetDifficulty(room)==2);
     ASSERT_TEST(roomGetDifficulty(room1)==1);
     ASSERT_TEST(roomGetDifficulty(room2)==10);
@@ -62,11 +62,11 @@ static bool testGetWorkingHours(){
     Room room= createRoom();
     Room room1= createRoom();
     int time[2];
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,20);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,20);
     (roomGetWorkingHours(room,time));
     ASSERT_TEST(time[0]==10);
     ASSERT_TEST(time[1]==20);
-    initRoom(&room1,"sha@s",23,2 ,"01-09",2,20);
+    initRoom(room1,"sha@s",23,2 ,"01-09",2,20);
     roomGetWorkingHours(room1,time);
     ASSERT_TEST(time[0]==1);
     ASSERT_TEST(time[1]==9);
@@ -79,9 +79,9 @@ static bool testGetWorkingHours(){
 static bool testGetPrice(){
     Room room= createRoom();
     Room room1= createRoom();
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,20);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,20);
     ASSERT_TEST(roomGetPrice(room)==20);
-    initRoom(&room1,"sha@s",23,2 ,"10-20",2,24);
+    initRoom(room1,"sha@s",23,2 ,"10-20",2,24);
     ASSERT_TEST(roomGetPrice(room1)==24);
     resetRoom(room);
     resetRoom(room1);
@@ -92,9 +92,9 @@ static bool testGetPrice(){
 static bool testGetEmail(){
     Room room= createRoom();
     Room room1= createRoom();
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,20);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,20);
     ASSERT_TEST(strcmp(roomGetEmail(room),"sha@s")==0);
-    initRoom(&room1,"sha@",23,2 ,"10-20",2,20);
+    initRoom(room1,"sha@",23,2 ,"10-20",2,20);
     ASSERT_TEST(strcmp(roomGetEmail(room1),"sha@")==0);
     resetRoom(room);
     resetRoom(room1);
@@ -106,10 +106,10 @@ static bool testCompareRoom(){
     Room room1= createRoom();
     Room room2= createRoom();
     Room room3= createRoom();
-    initRoom(&room,"sha@s",23,2 ,"10-20",2,24);
-    initRoom(&room1,"sha@s",23,2 ,"10-20",2,24);
-    initRoom(&room2,"sha@s",25,2 ,"10-20",2,24);
-    initRoom(&room3,"sha@s",22,2 ,"10-20",2,24);
+    initRoom(room,"sha@s",23,2 ,"10-20",2,24);
+    initRoom(room1,"sha@s",23,2 ,"10-20",2,24);
+    initRoom(room2,"sha@s",25,2 ,"10-20",2,24);
+    initRoom(room3,"sha@s",22,2 ,"10-20",2,24);
     ASSERT_TEST(compareRoom(room,room1)==0);
     ASSERT_TEST(compareRoom(room,room2)<0);
     ASSERT_TEST(compareRoom(room1,room3)>0);
@@ -127,8 +127,8 @@ static bool testCompareRoom(){
 static bool testCopyRoom(){
     Room room= createRoom();
     Room room1= createRoom();
-    initRoom(&room,"shsa@s",22,3 ,"11-20",3,20);
-    initRoom(&room1,"sha@s",23,2 ,"10-20",2,24);
+    initRoom(room,"shsa@s",22,3 ,"11-20",3,20);
+    initRoom(room1,"sha@s",23,2 ,"10-20",2,24);
     Room copy = copyRoom(room);
     Room copy1 = copyRoom(room1);
     ASSERT_TEST(compareRoom(room,copy)==0);
