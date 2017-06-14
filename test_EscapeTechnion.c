3e49cf6d4c39ac46987f693d55ca1d28611bd93e
shakedm <shakedmigdal@campus.technion.ac.il>
2017-06-14 03:43:50 +0300

    test

--- test_EscapeTechnion.c
+++ test_EscapeTechnion.c
@@ -28,6 +28,40 @@ static bool testCompanyRemove(){
     return true;
 }
 static bool testRoomAdd(){
+    EscapeTechnion system;
+    createEscapeTechnion(&system);
+    ASSERT_TEST(roomAdd(system,"as@s",2,4,2,"10-14",4)==ESCAPE_SUCCESS);
+    ASSERT_TEST(roomAdd(system,"as@s",2,4,2,"11-15",4)==ESCAPE_EMAIL_ALREADY_EXISTS);
+    ASSERT_TEST(roomAdd(system,"asd@s",3,4,2,"10-14",4)==ESCAPE_SUCCESS);
+    resetSystem(system);
+    return true;
 
 }
+static bool testRoomRemove(){
+    EscapeTechnion system;
+    createEscapeTechnion(&system);
+    companyAdd(system,"asd@a",COMPUTER_SCIENCE);
+    roomAdd(system,"asd@a",3,4,2,"10-20",4);
+    ASSERT_TEST(roomRemove(system,COMPUTER_SCIENCE,3)==ESCAPE_SUCCESS);
+    resetSystem(system);
+    return true;
+}
+static bool testEscaperAdd(){
+    EscapeTechnion system;
+    createEscapeTechnion(&system);
+    ASSERT_TEST(escaperAdd(system,"asd@s",PHYSICS,4)==ESCAPE_SUCCESS);
+    ASSERT_TEST(escaperAdd(system,"asd@s",COMPUTER_SCIENCE,4)==ESCAPE_EMAIL_ALREADY_EXISTS);
+    resetSystem(system);
+    return true;
+}
+static bool testEscaperRemove(){
+    EscapeTechnion system;
+    createEscapeTechnion(&system);
+    escaperAdd(system,"as@sa",PHYSICS,4);
+    ASSERT_TEST(escaperRemove(system,"as@sa")==ESCAPE_SUCCESS);
+    ASSERT_TEST(escaperRemove(system,"as@sa")==ESCAPE_CLIENT_EMAIL_DOES_NOT_EXIST);
+    resetSystem(system);
+    return true;
+}
+static bool test
 
